e93877616164a6298e7c689b2955efadeb24af9a
J <jieun1yoon@gmail.com>
2025-09-03 13:30:30 +0900

    vite.config.js 검토 및 전체 파일 최신 상태로 커밋

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,9 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-    plugins: [react()],
-    base: "/ComeTaebaek/", // 이 줄만 추가!
     server: {
         historyApiFallback: true,
     },
+    base: "/ComeTaebaek/",
+    plugins: [react()],
 });
